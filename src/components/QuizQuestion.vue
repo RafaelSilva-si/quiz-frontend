4bf24300b9a82ecdaf8942775ebc3bfdcfbca507
Rafael Mariano da Silva <77937182+RafaelSilva-si@users.noreply.github.com>
2023-02-03 14:07:52 -0300

    Feat: create question component

---
+++ src/components/QuizQuestion.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>What is the scientific name for modern day humans?</p>
+    <input type="radio" />Fit Description<br />
+    <input type="radio" />Fit Description<br />
+    <input type="radio" />Fit Description<br />
+    <input type="radio" />Fit Description<br />
+  </div>
+</template>
+
+<script></script>
+
+<style></style>

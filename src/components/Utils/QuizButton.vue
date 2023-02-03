76195f3e77dcc39fd666a16cbb6f5f897130fcb8
Rafael Mariano da Silva <77937182+RafaelSilva-si@users.noreply.github.com>
2023-02-03 14:08:05 -0300

    Feat: create button component

---
+++ src/components/Utils/QuizButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="button" type="button">Avançar</button>
+</template>
+
+<script></script>
+
+<style>
+.button {
+  border: none;
+  border-radius: 15px;
+  background-color: #e52729;
+  color: #fff;
+  height: 50px;
+  width: 90px;
+}
+</style>

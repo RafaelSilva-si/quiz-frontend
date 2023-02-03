3c2aed72fcfd7ffe55bc33cff781807cc5095970
Rafael Mariano da Silva <77937182+RafaelSilva-si@users.noreply.github.com>
2023-02-03 14:07:31 -0300

    Feat: create progress component

---
+++ src/components/QuizProgress.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="progress-container">
+    <div class="progress">
+      <div class="progress-bar" :style="{ width: 30 + '%' }"></div>
+    </div>
+    <p class="questionIndicator">Pergunta 1 de 20</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.progress {
+  width: 500px;
+  height: 10px;
+  border-radius: 50px;
+  background-color: #d1d5db;
+}
+
+.progress-bar {
+  width: 500px;
+  height: 10px;
+  border-radius: 50px;
+  background-color: #e52729;
+}
+</style>

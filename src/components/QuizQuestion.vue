41c77277dd55b4ec496e7a436016c1cef0f6a7ae
Rafael Mariano da Silva <77937182+RafaelSilva-si@users.noreply.github.com>
2023-02-06 07:02:56 -0300

    fix: refactoring question component

--- src/components/QuizQuestion.vue
+++ src/components/QuizQuestion.vue
@@ -1,37 +1,66 @@
 <template>
-  <div>
-    <p class="question">What is the scientific name for modern day humans?</p>
-    <form class="form">
-      <QuisInput :placeholder="'Fit Description'" />
-      <QuisInput :placeholder="'Fit Description'" />
-      <QuisInput :placeholder="'Fit Description'" />
-      <QuisInput :placeholder="'Fit Description'" />
+  <div class="form-element">
+    <p class="question">{{ question.question }}</p>
+    <form class="form" @submit="(e) => this.$emit('next', e)">
+      <div class="aswnser">
+        <QuisInput
+          v-for="(answers, i) in question.answers"
+          :key="i"
+          :placeholder="answers"
+          :value="answers"
+          :checked="question.aswnser == answers"
+        />
+      </div>
+      <a @click="this.$emit('back')" class="btn_back" v-if="current >= 1"
+        >Voltar</a
+      >
+      <QuizButton :title="'Avançar'" v-if="current <= 18" />
     </form>
   </div>
 </template>
 
 <script>
 import QuisInput from "./Utils/QuizInput.vue";
+import QuizButton from "./Utils/QuizButton.vue";
 
 export default {
   components: {
     QuisInput,
+    QuizButton,
+  },
+  data() {
+    return { feedback: "" };
+  },
+  props: {
+    question: { type: Object, required: true },
+    current: { type: Number, required: true },
   },
 };
 </script>
 
 <style>
+.form-element {
+  min-height: 200px;
+}
+.aswnser {
+  min-height: 100px;
+}
 .question {
-  font-size: 1.7rem;
-  line-height: 1px;
+  font-size: 1.2rem;
+  margin-top: -10px;
+  text-align: justify;
 }
 .form {
   margin-bottom: 30px;
 }
 
+.btn_back {
+  cursor: pointer;
+}
+
 @media (max-width: 600px) {
-  .question {
-    line-height: normal;
+  .form-element {
+    min-height: 300px;
   }
 }
 </style>
